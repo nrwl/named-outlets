a11574e08142c90abb29462abb770fac22b6b4f8
Victor Savkin <vic.savkin@gmail.com>
2018-10-26 09:41:19 -0400

    initial setup with two outlets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,78 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {Component, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {Router, RouterModule} from '@angular/router';
+
+@Component({
+  template: `
+    parent: <router-outlet></router-outlet>
+  `
+})
+export class ParentComponent {}
+
+@Component({
+  template: `
+    child1
+  `
+})
+export class Child1Component {}
+
+@Component({
+  template: `
+    child2
+  `
+})
+export class Child2Component {}
+
+@Component({
+  template: `
+    named1
+  `
+})
+export class Named1Component {}
+
+@Component({
+  template: `
+    named2
+  `
+})
+export class Named2Component {}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentComponent,
+    Child1Component,
+    Child2Component,
+    Named1Component,
+    Named2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'parent',
+        component: ParentComponent,
+        children: [
+          {path: 'child1', component: Child1Component},
+          {path: 'child2', component: Child2Component}
+        ]
+      },
+      {
+        path: '',
+        outlet: 'named',
+        component: Named2Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {
+    setTimeout(() => {
+      router.navigateByUrl('/parent/child1');
+    }, 0);
+  }
+}
